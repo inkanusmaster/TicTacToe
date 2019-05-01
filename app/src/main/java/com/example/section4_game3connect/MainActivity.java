25ea2640742051e64356d3bcf956c8b4660ea78c
inkanusmaster <furymistrz@gmail.com>
2019-05-01 18:19:57 +0200

    zostało sprawdzanie, punktacja, refaktoryzacja i takie takie

--- app/src/main/java/com/example/section4_game3connect/MainActivity.java
+++ app/src/main/java/com/example/section4_game3connect/MainActivity.java
@@ -58,105 +58,128 @@ public class MainActivity extends AppCompatActivity {
 
             if (view.getId()==(R.id.col_0_row_0)) {
                 col_0_row_0 = findViewById(R.id.col_0_row_0);
-                    if(col_0_row_0.getTag()!="empty") {
-                        Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Log.i("INFORMACJA", "YELLOW TOKEN ON CHART!");
-                        isYellowTurn = false;
-                        col_0_row_0.setTranslationY(-2000);
-                        col_0_row_0.setAlpha(1.0f);
-                        col_0_row_0.setImageResource(R.drawable.yellow);
-                        col_0_row_0.animate().translationYBy(2000).setDuration(1000);
-                        col_0_row_0.setTag("yellow");
-                    }
+                if(col_0_row_0.getTag()!="empty") {
+                    Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isYellowTurn = false;
+                    col_0_row_0.setTranslationY(-2000);
+                    col_0_row_0.setAlpha(1.0f);
+                    col_0_row_0.setImageResource(R.drawable.yellow);
+                    col_0_row_0.animate().translationYBy(2000).setDuration(1000);
+                    col_0_row_0.setTag("yellow");
+                }
             }
 
             if (view.getId()==(R.id.col_1_row_0)) {
-                Log.i("INFORMACJA", "YELLOW TOKEN ON CHART!");
-                isYellowTurn = false;
                 col_1_row_0 = findViewById(R.id.col_1_row_0);
-                col_1_row_0.setTranslationY(-2000);
-                col_1_row_0.setAlpha(1.0f);
-                col_1_row_0.setImageResource(R.drawable.yellow);
-                col_1_row_0.animate().translationYBy(2000).setDuration(1000);
-                col_1_row_0.setTag("yellow");
+                if(col_1_row_0.getTag()!="empty") {
+                    Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isYellowTurn = false;
+                    col_1_row_0.setTranslationY(-2000);
+                    col_1_row_0.setAlpha(1.0f);
+                    col_1_row_0.setImageResource(R.drawable.yellow);
+                    col_1_row_0.animate().translationYBy(2000).setDuration(1000);
+                    col_1_row_0.setTag("yellow");
+                }
             }
 
             if (view.getId()==(R.id.col_2_row_0)) {
-                Log.i("INFORMACJA", "YELLOW TOKEN ON CHART!");
-                isYellowTurn = false;
                 col_2_row_0 = findViewById(R.id.col_2_row_0);
-                col_2_row_0.setTranslationY(-2000);
-                col_2_row_0.setAlpha(1.0f);
-                col_2_row_0.setImageResource(R.drawable.yellow);
-                col_2_row_0.animate().translationYBy(2000).setDuration(1000);
-                col_2_row_0.setTag("yellow");
+                if(col_2_row_0.getTag()!="empty") {
+                    Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isYellowTurn = false;
+                    col_2_row_0.setTranslationY(-2000);
+                    col_2_row_0.setAlpha(1.0f);
+                    col_2_row_0.setImageResource(R.drawable.red);
+                    col_2_row_0.animate().translationYBy(2000).setDuration(1000);
+                    col_2_row_0.setTag("yellow");
+                }
             }
 
             if (view.getId()==(R.id.col_0_row_1)) {
-                Log.i("INFORMACJA", "YELLOW TOKEN ON CHART!");
-                isYellowTurn = false;
                 col_0_row_1 = findViewById(R.id.col_0_row_1);
-                col_0_row_1.setTranslationY(-2000);
-                col_0_row_1.setAlpha(1.0f);
-                col_0_row_1.setImageResource(R.drawable.yellow);
-                col_0_row_1.animate().translationYBy(2000).setDuration(1000);
-                col_0_row_1.setTag("yellow");
+                if(col_0_row_1.getTag()!="empty") {
+                    Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isYellowTurn = false;
+                    col_0_row_1.setTranslationY(-2000);
+                    col_0_row_1.setAlpha(1.0f);
+                    col_0_row_1.setImageResource(R.drawable.yellow);
+                    col_0_row_1.animate().translationYBy(2000).setDuration(1000);
+                    col_0_row_1.setTag("yellow");
+                }
             }
 
             if (view.getId()==(R.id.col_1_row_1)) {
-                Log.i("INFORMACJA", "YELLOW TOKEN ON CHART!");
-                isYellowTurn = false;
                 col_1_row_1 = findViewById(R.id.col_1_row_1);
-                col_1_row_1.setTranslationY(-2000);
-                col_1_row_1.setAlpha(1.0f);
-                col_1_row_1.setImageResource(R.drawable.yellow);
-                col_1_row_1.animate().translationYBy(2000).setDuration(1000);
-                col_1_row_1.setTag("yellow");
+                if(col_1_row_1.getTag()!="empty") {
+                    Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isYellowTurn = false;
+                    col_1_row_1.setTranslationY(-2000);
+                    col_1_row_1.setAlpha(1.0f);
+                    col_1_row_1.setImageResource(R.drawable.yellow);
+                    col_1_row_1.animate().translationYBy(2000).setDuration(1000);
+                    col_1_row_1.setTag("yellow");
+                }
             }
 
             if (view.getId()==(R.id.col_2_row_1)) {
-                Log.i("INFORMACJA", "YELLOW TOKEN ON CHART!");
-                isYellowTurn = false;
                 col_2_row_1 = findViewById(R.id.col_2_row_1);
-                col_2_row_1.setTranslationY(-2000);
-                col_2_row_1.setAlpha(1.0f);
-                col_2_row_1.setImageResource(R.drawable.yellow);
-                col_2_row_1.animate().translationYBy(2000).setDuration(1000);
-                col_2_row_1.setTag("yellow");
+                if(col_2_row_1.getTag()!="empty") {
+                    Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isYellowTurn = false;
+                    col_2_row_1.setTranslationY(-2000);
+                    col_2_row_1.setAlpha(1.0f);
+                    col_2_row_1.setImageResource(R.drawable.yellow);
+                    col_2_row_1.animate().translationYBy(2000).setDuration(1000);
+                    col_2_row_1.setTag("yellow");
+                }
             }
 
             if (view.getId()==(R.id.col_0_row_2)) {
-                Log.i("INFORMACJA", "YELLOW TOKEN ON CHART!");
-                isYellowTurn = false;
                 col_0_row_2 = findViewById(R.id.col_0_row_2);
-                col_0_row_2.setTranslationY(-2000);
-                col_0_row_2.setAlpha(1.0f);
-                col_0_row_2.setImageResource(R.drawable.yellow);
-                col_0_row_2.animate().translationYBy(2000).setDuration(1000);
-                col_0_row_2.setTag("yellow");
+                if(col_0_row_2.getTag()!="empty") {
+                    Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isYellowTurn = false;
+                    col_0_row_2.setTranslationY(-2000);
+                    col_0_row_2.setAlpha(1.0f);
+                    col_0_row_2.setImageResource(R.drawable.yellow);
+                    col_0_row_2.animate().translationYBy(2000).setDuration(1000);
+                    col_0_row_2.setTag("yellow");
+                }
             }
 
             if (view.getId()==(R.id.col_1_row_2)) {
-                Log.i("INFORMACJA", "YELLOW TOKEN ON CHART!");
-                isYellowTurn = false;
                 col_1_row_2 = findViewById(R.id.col_1_row_2);
-                col_1_row_2.setTranslationY(-2000);
-                col_1_row_2.setAlpha(1.0f);
-                col_1_row_2.setImageResource(R.drawable.yellow);
-                col_1_row_2.animate().translationYBy(2000).setDuration(1000);
-                col_1_row_2.setTag("yellow");
+                if(col_1_row_2.getTag()!="empty") {
+                    Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isYellowTurn = false;
+                    col_1_row_2.setTranslationY(-2000);
+                    col_1_row_2.setAlpha(1.0f);
+                    col_1_row_2.setImageResource(R.drawable.yellow);
+                    col_1_row_2.animate().translationYBy(2000).setDuration(1000);
+                    col_1_row_2.setTag("yellow");
+                }
             }
 
             if (view.getId()==(R.id.col_2_row_2)) {
-                Log.i("INFORMACJA", "YELLOW TOKEN ON CHART!");
-                isYellowTurn = false;
                 col_2_row_2 = findViewById(R.id.col_2_row_2);
-                col_2_row_2.setTranslationY(-2000);
-                col_2_row_2.setAlpha(1.0f);
-                col_2_row_2.setImageResource(R.drawable.yellow);
-                col_2_row_2.animate().translationYBy(2000).setDuration(1000);
-                col_2_row_2.setTag("yellow");
+                if(col_2_row_2.getTag()!="empty") {
+                    Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isYellowTurn = false;
+                    col_2_row_2.setTranslationY(-2000);
+                    col_2_row_2.setAlpha(1.0f);
+                    col_2_row_2.setImageResource(R.drawable.yellow);
+                    col_2_row_2.animate().translationYBy(2000).setDuration(1000);
+                    col_2_row_2.setTag("yellow");
+                }
             }
 
         } else {
@@ -166,7 +189,6 @@ public class MainActivity extends AppCompatActivity {
                 if(col_0_row_0.getTag()!="empty") {
                     Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Log.i("INFORMACJA", "RED TOKEN ON CHART!");
                     isYellowTurn = true;
                     col_0_row_0.setTranslationY(-2000);
                     col_0_row_0.setAlpha(1.0f);
@@ -191,121 +213,109 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-            if (view.getId()==(R.id.col_0_row_0)) {
-                col_0_row_0 = findViewById(R.id.col_0_row_0);
-                if(col_0_row_0.getTag()!="empty") {
+            if (view.getId()==(R.id.col_2_row_0)) {
+                col_2_row_0 = findViewById(R.id.col_2_row_0);
+                if(col_2_row_0.getTag()!="empty") {
                     Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Log.i("INFORMACJA", "RED TOKEN ON CHART!");
                     isYellowTurn = true;
-                    col_0_row_0.setTranslationY(-2000);
-                    col_0_row_0.setAlpha(1.0f);
-                    col_0_row_0.setImageResource(R.drawable.red);
-                    col_0_row_0.animate().translationYBy(2000).setDuration(1000);
-                    col_0_row_0.setTag("red");
+                    col_2_row_0.setTranslationY(-2000);
+                    col_2_row_0.setAlpha(1.0f);
+                    col_2_row_0.setImageResource(R.drawable.red);
+                    col_2_row_0.animate().translationYBy(2000).setDuration(1000);
+                    col_2_row_0.setTag("red");
                 }
             }
 
-            if (view.getId()==(R.id.col_0_row_0)) {
-                col_0_row_0 = findViewById(R.id.col_0_row_0);
-                if(col_0_row_0.getTag()!="empty") {
+            if (view.getId()==(R.id.col_0_row_1)) {
+                col_0_row_1 = findViewById(R.id.col_0_row_1);
+                if(col_0_row_1.getTag()!="empty") {
                     Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Log.i("INFORMACJA", "RED TOKEN ON CHART!");
                     isYellowTurn = true;
-                    col_0_row_0.setTranslationY(-2000);
-                    col_0_row_0.setAlpha(1.0f);
-                    col_0_row_0.setImageResource(R.drawable.red);
-                    col_0_row_0.animate().translationYBy(2000).setDuration(1000);
-                    col_0_row_0.setTag("red");
+                    col_0_row_1.setTranslationY(-2000);
+                    col_0_row_1.setAlpha(1.0f);
+                    col_0_row_1.setImageResource(R.drawable.red);
+                    col_0_row_1.animate().translationYBy(2000).setDuration(1000);
+                    col_0_row_1.setTag("red");
                 }
             }
 
-            if (view.getId()==(R.id.col_0_row_0)) {
-                col_0_row_0 = findViewById(R.id.col_0_row_0);
-                if(col_0_row_0.getTag()!="empty") {
+            if (view.getId()==(R.id.col_1_row_1)) {
+                col_1_row_1 = findViewById(R.id.col_1_row_1);
+                if(col_1_row_1.getTag()!="empty") {
                     Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Log.i("INFORMACJA", "RED TOKEN ON CHART!");
                     isYellowTurn = true;
-                    col_0_row_0.setTranslationY(-2000);
-                    col_0_row_0.setAlpha(1.0f);
-                    col_0_row_0.setImageResource(R.drawable.red);
-                    col_0_row_0.animate().translationYBy(2000).setDuration(1000);
-                    col_0_row_0.setTag("red");
+                    col_1_row_1.setTranslationY(-2000);
+                    col_1_row_1.setAlpha(1.0f);
+                    col_1_row_1.setImageResource(R.drawable.red);
+                    col_1_row_1.animate().translationYBy(2000).setDuration(1000);
+                    col_1_row_1.setTag("red");
                 }
             }
 
-            if (view.getId()==(R.id.col_0_row_0)) {
-                col_0_row_0 = findViewById(R.id.col_0_row_0);
-                if(col_0_row_0.getTag()!="empty") {
+            if (view.getId()==(R.id.col_2_row_1)) {
+                col_2_row_1 = findViewById(R.id.col_2_row_1);
+                if(col_2_row_1.getTag()!="empty") {
                     Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Log.i("INFORMACJA", "RED TOKEN ON CHART!");
                     isYellowTurn = true;
-                    col_0_row_0.setTranslationY(-2000);
-                    col_0_row_0.setAlpha(1.0f);
-                    col_0_row_0.setImageResource(R.drawable.red);
-                    col_0_row_0.animate().translationYBy(2000).setDuration(1000);
-                    col_0_row_0.setTag("red");
+                    col_2_row_1.setTranslationY(-2000);
+                    col_2_row_1.setAlpha(1.0f);
+                    col_2_row_1.setImageResource(R.drawable.red);
+                    col_2_row_1.animate().translationYBy(2000).setDuration(1000);
+                    col_2_row_1.setTag("red");
                 }
             }
 
-            if (view.getId()==(R.id.col_0_row_0)) {
-                col_0_row_0 = findViewById(R.id.col_0_row_0);
-                if(col_0_row_0.getTag()!="empty") {
+            if (view.getId()==(R.id.col_0_row_2)) {
+                col_0_row_2 = findViewById(R.id.col_0_row_2);
+                if(col_0_row_2.getTag()!="empty") {
                     Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Log.i("INFORMACJA", "RED TOKEN ON CHART!");
                     isYellowTurn = true;
-                    col_0_row_0.setTranslationY(-2000);
-                    col_0_row_0.setAlpha(1.0f);
-                    col_0_row_0.setImageResource(R.drawable.red);
-                    col_0_row_0.animate().translationYBy(2000).setDuration(1000);
-                    col_0_row_0.setTag("red");
+                    col_0_row_2.setTranslationY(-2000);
+                    col_0_row_2.setAlpha(1.0f);
+                    col_0_row_2.setImageResource(R.drawable.red);
+                    col_0_row_2.animate().translationYBy(2000).setDuration(1000);
+                    col_0_row_2.setTag("red");
                 }
             }
 
-            if (view.getId()==(R.id.col_0_row_0)) {
-                col_0_row_0 = findViewById(R.id.col_0_row_0);
-                if(col_0_row_0.getTag()!="empty") {
+            if (view.getId()==(R.id.col_1_row_2)) {
+                col_1_row_2 = findViewById(R.id.col_1_row_2);
+                if(col_1_row_2.getTag()!="empty") {
                     Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Log.i("INFORMACJA", "RED TOKEN ON CHART!");
                     isYellowTurn = true;
-                    col_0_row_0.setTranslationY(-2000);
-                    col_0_row_0.setAlpha(1.0f);
-                    col_0_row_0.setImageResource(R.drawable.red);
-                    col_0_row_0.animate().translationYBy(2000).setDuration(1000);
-                    col_0_row_0.setTag("red");
+                    col_1_row_2.setTranslationY(-2000);
+                    col_1_row_2.setAlpha(1.0f);
+                    col_1_row_2.setImageResource(R.drawable.red);
+                    col_1_row_2.animate().translationYBy(2000).setDuration(1000);
+                    col_1_row_2.setTag("red");
                 }
             }
 
-            if (view.getId()==(R.id.col_0_row_0)) {
-                col_0_row_0 = findViewById(R.id.col_0_row_0);
-                if(col_0_row_0.getTag()!="empty") {
+            if (view.getId()==(R.id.col_2_row_2)) {
+                col_2_row_2 = findViewById(R.id.col_2_row_2);
+                if(col_2_row_2.getTag()!="empty") {
                     Toast.makeText(this, "Choose other field!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Log.i("INFORMACJA", "RED TOKEN ON CHART!");
                     isYellowTurn = true;
-                    col_0_row_0.setTranslationY(-2000);
-                    col_0_row_0.setAlpha(1.0f);
-                    col_0_row_0.setImageResource(R.drawable.red);
-                    col_0_row_0.animate().translationYBy(2000).setDuration(1000);
-                    col_0_row_0.setTag("red");
+                    col_2_row_2.setTranslationY(-2000);
+                    col_2_row_2.setAlpha(1.0f);
+                    col_2_row_2.setImageResource(R.drawable.red);
+                    col_2_row_2.animate().translationYBy(2000).setDuration(1000);
+                    col_2_row_2.setTag("red");
                 }
             }
-
-
-
-
-
-
-
         }
-
     }
 
+    public void checkGameState(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
